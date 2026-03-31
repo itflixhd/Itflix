c1aae91b23716ca83f391770d28457b0f2e07a06
itflixhd <a61555402@gmail.com>
2026-03-31 16:32:35 -0300

    Delete iptv directory

--- iptv/teste.php
+++
@@ -1,7 +0,0 @@
-#EXTM3U url-tvg="https://epg.tvnow.best/epg.xml"
-
-#EXTINF:-1 tvg-id="teste1" tvg-name="Canal Teste 1" group-title="TESTE",Canal Teste 1
-https://raw.githubusercontent.com/itflixhd/Itflix/refs/heads/main/iptv.m3u8
-
-#EXTINF:-1 tvg-id="teste2" tvg-name="Canal Teste 2" group-title="TESTE",Canal Teste 2
-https://raw.githubusercontent.com/itflixhd/Iptvz/refs/heads/main/lista_filtrada.m3u8
